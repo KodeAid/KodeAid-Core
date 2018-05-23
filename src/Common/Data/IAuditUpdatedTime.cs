265f4d9157ba4b57f6a989c5d47417f30d212850
Kris Penner <krispenner@hotmail.com>
2018-05-23 00:17:20 -0600

    Add data interfaces (auditing and optimistic concurrency).

---
+++ src/Common/Data/IAuditUpdatedTime.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+
+namespace KodeAid.Data
+{
+    public interface IAuditUpdatedTime
+    {
+        DateTimeOffset? UpdatedAt { get; set; }
+    }
+}
---
+++ src/Common/Data/IOptimisticConcurrency.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+
+namespace KodeAid.Data
+{
+    public interface IOptimisticConcurrency
+    {
+        // ConcurrencyToken, Timestamp, Version, RowVersion
+        object ConcurrencyStamp { get; set; }
+    }
+}
