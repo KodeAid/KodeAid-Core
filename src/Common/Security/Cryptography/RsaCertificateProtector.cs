d4354502eef12ccabf9cdd87991615c053f48a87
Kris Penner <krispenner@hotmail.com>
2018-05-23 16:16:04 -0600

    Add RSA certificate IDataProtector implementation.

---
+++ src/Common/Security/Cryptography/RsaCertificateProtector.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using KodeAid.Security.Cryptography.X509Certificates;
+
+namespace KodeAid.Security.Cryptography
+{
+    public class RsaCertificateProtector : IDataProtector
+    {
+        private readonly X509Certificate2 _certificate;
+        private readonly RSAEncryptionPadding _padding;
+
+        public RsaCertificateProtector(X509Certificate2 certificate)
+            : this(certificate, RSAEncryptionPadding.OaepSHA1)
+        {
+        }
+
+        public RsaCertificateProtector(X509Certificate2 certificate, RSAEncryptionPadding padding)
+        {
+            ArgCheck.NotNull(nameof(certificate), certificate);
+            _certificate = certificate;
+        }
+
+        public byte[] ProtectData(byte[] unprotectedData)
+        {
+            ArgCheck.NotNull(nameof(unprotectedData), unprotectedData);
+            return _certificate.GetRSAPrivateKey().Encrypt(unprotectedData, _padding);
+        }
+
+        public byte[] UnprotectData(byte[] protectedData)
+        {
+            ArgCheck.NotNull(nameof(protectedData), protectedData);
+            return _certificate.GetRSAPrivateKey().Decrypt(protectedData, _padding);
+        }
+    }
+}
