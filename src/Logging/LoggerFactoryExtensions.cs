bf157f164a0943dbd7606d0171dd2eab5d4d6b70
Kris Penner <krispenner@hotmail.com>
2018-05-22 19:21:50 -0600

    Add back LoggerFactory extensions to create NopLogger if factory is null.

---
+++ src/Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using KodeAid.Logging;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class LoggerFactoryExtensions
+    {
+        public static ILogger CreateLoggerOrNopIfNull(this ILoggerFactory loggerFactory, string categoryName)
+        {
+            return loggerFactory?.CreateLogger(categoryName) ?? NopLogger.Instance;
+        }
+
+        public static ILogger<T> CreateLoggerOrNopIfNull<T>(this ILoggerFactory loggerFactory)
+        {
+            return loggerFactory?.CreateLogger<T>() ?? NopLogger<T>.Instance;
+        }
+    }
+}
