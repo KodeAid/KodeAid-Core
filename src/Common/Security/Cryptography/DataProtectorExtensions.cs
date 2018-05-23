83a9a7e29d8a9f735f9b084a876b23219c462afb
Kris Penner <krispenner@hotmail.com>
2018-05-23 16:16:36 -0600

    Clean-up IDataProtector extension methods.

--- src/Common/Security/Cryptography/DataProtectorExtensions.cs
+++ src/Common/Security/Cryptography/DataProtectorExtensions.cs
@@ -3,44 +3,146 @@
 
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace KodeAid.Security.Cryptography
 {
     public static class DataProtectorExtensions
     {
-        private static readonly Encoding _defaultEncoding = Encoding.UTF8;
+        internal static readonly Encoding _defaultEncoding = Encoding.UTF8;
 
-        public static string EncryptStringToBase64(this IDataProtector dataProtector, string textToEncrypt, string password, bool urlEncoded = false, Encoding encoding = null)
+        public static byte[] ProtectString(this IDataProtector dataProtector, string unprotectedString, Encoding encoding = null)
         {
             ArgCheck.NotNull(nameof(dataProtector), dataProtector);
-            ArgCheck.NotNullOrEmpty(nameof(textToEncrypt), textToEncrypt);
-            ArgCheck.NotNullOrEmpty(nameof(password), password);
-            return EncryptDataToBase64(dataProtector, (encoding ?? _defaultEncoding).GetBytes(textToEncrypt), password, urlEncoded, encoding);
+            ArgCheck.NotNull(nameof(unprotectedString), unprotectedString);
+            return dataProtector.ProtectData((encoding ?? _defaultEncoding).GetBytes(unprotectedString));
         }
 
-        public static string DecryptStringFromBase64(this IDataProtector dataProtector, string base64ToDecrypt, string password, bool urlEncoded = false, Encoding encoding = null)
+        public static byte[] ProtectStrings(this IDataProtector dataProtector, string[] unprotectedStrings, Encoding encoding = null)
         {
             ArgCheck.NotNull(nameof(dataProtector), dataProtector);
-            ArgCheck.NotNullOrEmpty(nameof(base64ToDecrypt), base64ToDecrypt);
-            ArgCheck.NotNullOrEmpty(nameof(password), password);
-            return (encoding ?? _defaultEncoding).GetString(DecryptDataFromBase64(dataProtector, base64ToDecrypt, password, urlEncoded, encoding));
+            ArgCheck.NotNull(nameof(unprotectedStrings), unprotectedStrings);
+            return dataProtector.ProtectData(GetBytesFromStrings(unprotectedStrings, encoding ?? _defaultEncoding));
         }
 
-        public static string EncryptDataToBase64(this IDataProtector dataProtector, byte[] dataToEncrypt, string password, bool urlEncoded = false, Encoding encoding = null)
+        public static byte[] ProtectStrings(this IDataProtector dataProtector, params string[] unprotectedStrings)
         {
-            ArgCheck.NotNull(nameof(dataProtector), dataProtector);
-            ArgCheck.NotNullOrEmpty(nameof(dataToEncrypt), dataToEncrypt);
-            ArgCheck.NotNullOrEmpty(nameof(password), password);
-            return dataProtector.EncryptData(dataToEncrypt, (encoding ?? _defaultEncoding).GetBytes(password)).ToBase64(urlEncoded);
+            return dataProtector.ProtectStrings(unprotectedStrings, null);
         }
 
-        public static byte[] DecryptDataFromBase64(this IDataProtector dataProtector, string base64ToDecrypt, string password, bool urlEncoded = false, Encoding encoding = null)
+        public static byte[] ProtectStrings(this IDataProtector dataProtector, Encoding encoding, params string[] unprotectedStrings)
+        {
+            return dataProtector.ProtectStrings(unprotectedStrings, encoding);
+        }
+
+        public static string UnprotectString(this IDataProtector dataProtector, byte[] protectedData, Encoding encoding = null)
         {
             ArgCheck.NotNull(nameof(dataProtector), dataProtector);
-            ArgCheck.NotNullOrEmpty(nameof(base64ToDecrypt), base64ToDecrypt);
-            ArgCheck.NotNullOrEmpty(nameof(password), password);
-            return dataProtector.DecryptData(base64ToDecrypt.FromBase64(urlEncoded), (encoding ?? _defaultEncoding).GetBytes(password));
+            ArgCheck.NotNull(nameof(protectedData), protectedData);
+            return (encoding ?? _defaultEncoding).GetString(dataProtector.UnprotectData(protectedData));
+        }
+
+        public static string[] UnprotectStrings(this IDataProtector dataProtector, byte[] protectedData, Encoding encoding = null)
+        {
+            ArgCheck.NotNull(nameof(dataProtector), dataProtector);
+            ArgCheck.NotNull(nameof(protectedData), protectedData);
+            return GetStringsFromBytes(dataProtector.UnprotectData(protectedData), encoding ?? _defaultEncoding);
+        }
+
+        public static string ProtectDataToBase64(this IDataProtector dataProtector, byte[] unprotectedData, Encoding encoding = null, bool urlEncoded = false)
+        {
+            return dataProtector.ProtectData(unprotectedData).ToBase64(urlEncoded);
+        }
+
+        public static string ProtectStringToBase64(this IDataProtector dataProtector, string unprotectedString, Encoding encoding = null, bool urlEncoded = false)
+        {
+            return dataProtector.ProtectString(unprotectedString, encoding).ToBase64(urlEncoded);
+        }
+
+        public static string ProtectStringsToBase64(this IDataProtector dataProtector, string[] unprotectedStrings, Encoding encoding = null, bool urlEncoded = false)
+        {
+            return dataProtector.ProtectStrings(unprotectedStrings, encoding).ToBase64(urlEncoded);
+        }
+
+        public static string ProtectStringsToBase64(this IDataProtector dataProtector, bool urlEncoded = false, params string[] unprotectedStrings)
+        {
+            return dataProtector.ProtectStrings(unprotectedStrings, null).ToBase64(urlEncoded);
+        }
+
+        public static string ProtectStringsToBase64(this IDataProtector dataProtector, Encoding encoding, params string[] unprotectedStrings)
+        {
+            return dataProtector.ProtectStrings(unprotectedStrings, encoding).ToBase64();
+        }
+
+        public static string ProtectStringsToBase64(this IDataProtector dataProtector, Encoding encoding, bool urlEncoded, params string[] unprotectedStrings)
+        {
+            return dataProtector.ProtectStrings(unprotectedStrings, encoding).ToBase64(urlEncoded);
+        }
+
+        public static byte[] UnprotectDataFromBase64(this IDataProtector dataProtector, string protectedBase64, Encoding encoding = null, bool urlEncoded = false)
+        {
+            ArgCheck.NotNull(nameof(protectedBase64), protectedBase64);
+            return dataProtector.UnprotectData(protectedBase64.FromBase64(urlEncoded));
+        }
+
+        public static string UnprotectStringFromBase64(this IDataProtector dataProtector, string protectedBase64, Encoding encoding = null, bool urlEncoded = false)
+        {
+            ArgCheck.NotNull(nameof(protectedBase64), protectedBase64);
+            return dataProtector.UnprotectString(protectedBase64.FromBase64(urlEncoded), encoding);
+        }
+
+        public static string[] UnprotectStringsFromBase64(this IDataProtector dataProtector, string protectedBase64, Encoding encoding = null, bool urlEncoded = false)
+        {
+            ArgCheck.NotNull(nameof(protectedBase64), protectedBase64);
+            return dataProtector.UnprotectStrings(protectedBase64.FromBase64(urlEncoded), encoding);
+        }
+
+        private static byte[] GetBytesFromStrings(string[] values, Encoding encoding)
+        {
+            ArgCheck.NotNull(nameof(values), values);
+            ArgCheck.NotNull(nameof(encoding), encoding);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var w = new BinaryWriter(ms, encoding, true))
+                {
+                    w.Write(values.Count());
+                    foreach (var v in values)
+                    {
+                        w.Write(v != null);
+                        if (v != null)
+                            w.Write(v);
+                    }
+                    w.Flush();
+                }
+                ms.Flush();
+                return ms.ToArray();
+            }
+        }
+
+        private static string[] GetStringsFromBytes(byte[] data, Encoding encoding)
+        {
+            ArgCheck.NotNullOrEmpty(nameof(data), data);
+            ArgCheck.NotNull(nameof(encoding), encoding);
+
+            var values = new List<string>();
+            using (var ms = new MemoryStream(data))
+            {
+                using (var r = new BinaryReader(ms, encoding, true))
+                {
+                    for (var i = r.ReadInt32(); i > 0; --i)
+                    {
+                        if (r.ReadBoolean())
+                            values.Add(r.ReadString());
+                        else
+                            values.Add(null);
+                    }
+                }
+            }
+            return values.ToArray();
         }
     }
 }
