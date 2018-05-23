e6bc14161855e3be8d9e66b038a50ff3ca4d9283
Kris Penner <krispenner@hotmail.com>
2018-05-23 00:37:53 -0600

    Added AspNetCore project.

---
+++ src/AspNetCore/RewriteOptionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using KodeAid.AspNetCore.Rewrite;
+
+namespace Microsoft.AspNetCore.Rewrite
+{
+    public static class RewriteOptionsExtensions
+    {
+        public static RewriteOptions AddRedirectToWww(this RewriteOptions options)
+        {
+            return options.Add(new WwwRule());
+        }
+    }
+}
---
+++ src/AspNetCore/WwwRule.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace KodeAid.AspNetCore.Rewrite
+{
+    public class WwwRule : IRule
+    {
+        public void ApplyRule(RewriteContext context)
+        {
+            var host = context?.HttpContext?.Request?.Host.Host;
+            if (!string.IsNullOrEmpty(host) && !host.StartsWith("www.", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var url = context.HttpContext.Request.GetDisplayUrl();
+                context.HttpContext.Response.Redirect(url.Insert(url.IndexOf("://") + 3, "www."), true);
+                context.Result = RuleResult.EndResponse;
+            }
+        }
+    }
+}
