340678ec81fcdc2d7eda57a17a6198a7f477b31c
Kris Penner <krispenner@hotmail.com>
2018-05-23 23:23:20 -0600

    Update copyright header.

--- src/Common/Net/DnsHelper.cs
+++ src/Common/Net/DnsHelper.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Kris Penner. All rights reserved.
+﻿// Copyright © Kris Penner. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Net;
