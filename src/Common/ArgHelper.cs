7ff9294a6b9a942ee4eb0ba9595d5020b235e07e
Kris Penner <krispenner@hotmail.com>
2018-05-23 23:10:23 -0600

    Add ArgHelper with Clamp() method.

---
+++ src/Common/ArgHelper.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+
+namespace KodeAid
+{
+    public static class ArgHelper
+    {
+        public static int Clamp(int value, int minValue, int maxValue)
+        {
+            return Math.Min(Math.Max(value, minValue), maxValue);
+        }
+    }
+}
