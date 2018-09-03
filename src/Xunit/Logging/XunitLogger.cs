873d621d579ccc718fddc27dfa4317180203878e
Kris Penner <krispenner@hotmail.com>
2018-09-03 14:53:56 -0700

    Add XUnit logging helpers.

---
+++ src/Xunit/Logging/XunitLogger.cs
@@ -0,0 +1,38 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace KodeAid.Testing.Xunit.Logging
+{
+    public class XunitLogger : ILogger
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly string _categoryName;
+
+        public XunitLogger(ITestOutputHelper testOutputHelper, string categoryName)
+        {
+            _testOutputHelper = testOutputHelper;
+            _categoryName = categoryName;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+            => Disposable.Nop;
+
+        public bool IsEnabled(LogLevel logLevel)
+            => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (eventId.Id == default)
+                _testOutputHelper.WriteLine($"{_categoryName}: {formatter(state, exception)}");
+            else
+                _testOutputHelper.WriteLine($"{_categoryName}: [{eventId}] {formatter(state, exception)}");
+            if (exception != null)
+                _testOutputHelper.WriteLine(exception.ToString());
+        }
+    }
+}
---
+++ src/Xunit/Logging/XunitLoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace KodeAid.Testing.Xunit.Logging
+{
+    public class XunitLoggerProvider : ILoggerProvider
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public XunitLoggerProvider(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+            => new XunitLogger(_testOutputHelper, categoryName);
+
+        public void Dispose()
+        { }
+    }
+}
