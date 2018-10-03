a2cb7c8fbc0d9c79d2046193c7a7385c52801ca6
Kris Penner <krispenner@hotmail.com>
2018-10-03 02:45:00 -0700

    Add XmlSerializerHelper serializing objects to a string supporting common requirements.

---
+++ src/Common/Xml/Serialization/XmlSerializerHelper.cs
@@ -0,0 +1,49 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using KodeAid.IO;
+
+namespace KodeAid.Xml.Serialization
+{
+    public static class XmlSerializerHelper
+    {
+        public static string Serialize<T>(T obj, bool indent = false, bool omitStandardNamespaces = false, string defaultNamespace = null, bool omitXmlDeclaration = false, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            var settings = new XmlWriterSettings()
+            {
+                Encoding = Encoding.UTF8,
+                Indent = indent,
+                OmitXmlDeclaration = omitXmlDeclaration,
+            };
+
+            var ns = new XmlSerializerNamespaces();
+            if (omitStandardNamespaces)
+            {
+                ns.Add("", "");
+            }
+            if (defaultNamespace != null)
+            {
+                ns.Add("", defaultNamespace);
+            }
+
+            var sb = new StringBuilder();
+            using (var sw = new StringWriter(encoding, sb))
+            using (var xw = XmlWriter.Create(sw, settings))
+            {
+                new XmlSerializer(typeof(T), defaultNamespace).Serialize(xw, obj, ns);
+                xw.Flush();
+                sw.Flush();
+            }
+            return sb.ToString();
+        }
+    }
+}
