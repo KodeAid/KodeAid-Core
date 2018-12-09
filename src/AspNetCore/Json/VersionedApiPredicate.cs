07b627b1b7e1581757523e804c676d242c869888
Kris Penner <krispenner@hotmail.com>
2018-12-09 11:04:39 -0800

    Pass down missed constructor parameter for VersionedApiPredicate.

--- src/AspNetCore/Json/VersionedApiPredicate.cs
+++ src/AspNetCore/Json/VersionedApiPredicate.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 
-using System;
 using System.Collections.Generic;
 using KodeAid.Serialization.Json.ContractResolvers;
 using Microsoft.AspNetCore.Http;
@@ -12,8 +11,8 @@ namespace KodeAid.AspNetCore.Json
 {
     public class VersionedApiPredicate : VersionedModelPredicate
     {
-        public VersionedApiPredicate(IHttpContextAccessor httpContextAccessor, IEqualityComparer<Version> comparer = null)
-            : base(() => httpContextAccessor.HttpContext.GetRequestedApiVersion()?.ToString())
+        public VersionedApiPredicate(IHttpContextAccessor httpContextAccessor, IEqualityComparer<string> comparer = null)
+            : base(() => httpContextAccessor.HttpContext.GetRequestedApiVersion()?.ToString(), comparer)
         {
         }
     }
