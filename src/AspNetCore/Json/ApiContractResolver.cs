a9909465224e5598a9fcdf861c00df386f62955a
Kris Penner <krispenner@hotmail.com>
2018-12-10 11:22:09 -0800

    Add ApiContractResolver.

---
+++ src/AspNetCore/Json/ApiContractResolver.cs
@@ -0,0 +1,20 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using KodeAid.Serialization.Json.ContractResolvers;
+using Microsoft.AspNetCore.Http;
+
+namespace KodeAid.AspNetCore.Json
+{
+    public class ApiContractResolver : PredicateContractResolver
+    {
+        public ApiContractResolver(IHttpContextAccessor httpContextAccessor)
+        {
+            ArgCheck.NotNull(nameof(httpContextAccessor), httpContextAccessor);
+
+            Add(new EmptyArrayPredicate());
+            Add(new VersionedApiPredicate(httpContextAccessor));
+        }
+    }
+}
