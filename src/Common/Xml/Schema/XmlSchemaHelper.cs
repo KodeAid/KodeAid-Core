d927cf85912b5b09c68649495d773eaa0a0dddb7
Kris Penner <krispenner@hotmail.com>
2019-01-02 12:31:20 -0800

    Update XML schema type parsing.

---
+++ src/Common/Xml/Schema/XmlSchemaHelper.cs
@@ -0,0 +1,175 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Xml;
+
+namespace KodeAid.Xml.Schema
+{
+    public static class XmlSchemaHelper
+    {
+        public static Type GetTypeFromSchema(string schemaType)
+        {
+            ArgCheck.NotNullOrEmpty(nameof(schemaType), schemaType);
+
+            if (!TryGetTypeFromSchema(schemaType, out var type))
+            {
+                throw new ArgumentException($"Unknown XML schema type: {schemaType}.", nameof(schemaType));
+            }
+
+            return type;
+        }
+
+        public static Type GetTypeOrDefaultFromSchema(string schemaType, Type defaultType = null)
+        {
+            return TryGetTypeFromSchema(schemaType, out var type) ? type : defaultType;
+        }
+
+        public static bool TryGetTypeFromSchema(string schemaType, out Type type)
+        {
+            // https://msdn.microsoft.com/en-us/library/aa719879(v=vs.71).aspx
+            switch (schemaType?.ToLower())
+            {
+                case XmlSchemaTypes.AnyUri:
+                    type = typeof(Uri);
+                    return true;
+                case XmlSchemaTypes.Base64Binary:
+                    type = typeof(byte[]);  // string??
+                    return true;
+                case XmlSchemaTypes.Boolean:
+                    type = typeof(bool);
+                    return true;
+                case XmlSchemaTypes.Byte:
+                    type = typeof(sbyte);
+                    return true;
+                case XmlSchemaTypes.Date:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.Datetime:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.Decimal:
+                    type = typeof(decimal);
+                    return true;
+                case XmlSchemaTypes.Double:
+                    type = typeof(double);
+                    return true;
+                case XmlSchemaTypes.Duration:
+                    type = typeof(TimeSpan);
+                    return true;
+                case XmlSchemaTypes.Entities:
+                    type = typeof(string[]);
+                    return true;
+                case XmlSchemaTypes.Entity:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.Float:
+                    type = typeof(float);
+                    return true;
+                case XmlSchemaTypes.Gday:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.GMonthDay:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.GYear:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.GYearMonth:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.HexBinary:
+                    type = typeof(byte[]);
+                    return true;
+                case XmlSchemaTypes.Id:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.IdRef:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.IdRefs:
+                    type = typeof(string[]);
+                    return true;
+                case XmlSchemaTypes.Int:
+                    type = typeof(int);
+                    return true;
+                case XmlSchemaTypes.Integer:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.Language:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.Long:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.Month:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.Name:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.NcName:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.NegativeInteger:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.NmToken:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.NmTokens:
+                    type = typeof(string[]);
+                    return true;
+                case XmlSchemaTypes.NonNegativeInteger:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.NonPositiveInteger:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.NormalizedString:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.Notation:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.PositiveInteger:
+                    type = typeof(long);
+                    return true;
+                case XmlSchemaTypes.QName:
+                    type = typeof(XmlQualifiedName);
+                    return true;
+                case XmlSchemaTypes.Short:
+                    type = typeof(short);
+                    return true;
+                case XmlSchemaTypes.String:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.Time:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.TimePeriod:
+                    type = typeof(DateTime);
+                    return true;
+                case XmlSchemaTypes.Token:
+                    type = typeof(string);
+                    return true;
+                case XmlSchemaTypes.UnsignedByte:
+                    type = typeof(byte);
+                    return true;
+                case XmlSchemaTypes.UnsignedInt:
+                    type = typeof(uint);
+                    return true;
+                case XmlSchemaTypes.UnsignedLong:
+                    type = typeof(ulong);
+                    return true;
+                case XmlSchemaTypes.UnsignedShort:
+                    type = typeof(ushort);
+                    return true;
+                default:
+                    type = null;
+                    return false;
+            }
+        }
+    }
+}
---
+++ src/Common/Xml/Schema/XmlSchemaTypes.cs
@@ -0,0 +1,55 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+namespace KodeAid.Xml.Schema
+{
+    public static class XmlSchemaTypes
+    {
+        public const string AnyUri = "anyuri";
+        public const string Base64Binary = "base64binary";
+        public const string Boolean = "boolean";
+        public const string Byte = "byte";
+        public const string Date = "date";
+        public const string Datetime = "datetime";
+        public const string Decimal = "decimal";
+        public const string Double = "double";
+        public const string Duration = "duration";
+        public const string Entities = "entities";
+        public const string Entity = "entity";
+        public const string Float = "float";
+        public const string Gday = "gday";
+        public const string GMonthDay = "gmonthday";
+        public const string GYear = "gyear";
+        public const string GYearMonth = "gyearmonth";
+        public const string HexBinary = "hexbinary";
+        public const string Id = "id";
+        public const string IdRef = "idref";
+        public const string IdRefs = "idrefs";
+        public const string Int = "int";
+        public const string Integer = "integer";
+        public const string Language = "language";
+        public const string Long = "long";
+        public const string Month = "month";
+        public const string Name = "name";
+        public const string NcName = "ncname";
+        public const string NegativeInteger = "negativeinteger";
+        public const string NmToken = "nmtoken";
+        public const string NmTokens = "nmtokens";
+        public const string NonNegativeInteger = "nonnegativeinteger";
+        public const string NonPositiveInteger = "nonpositiveinteger";
+        public const string NormalizedString = "normalizedstring";
+        public const string Notation = "notation";
+        public const string PositiveInteger = "positiveinteger";
+        public const string QName = "qname";
+        public const string Short = "short";
+        public const string String = "string";
+        public const string Time = "time";
+        public const string TimePeriod = "timeperiod";
+        public const string Token = "token";
+        public const string UnsignedByte = "unsignedbyte";
+        public const string UnsignedInt = "unsignedint";
+        public const string UnsignedLong = "unsignedlong";
+        public const string UnsignedShort = "unsignedshort";
+    }
+}
