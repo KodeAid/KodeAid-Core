c7ec0aded64cfe56e3c2ce0af57b279e94305024
Kris Penner <krispenner@hotmail.com>
2019-03-01 13:30:36 -0800

    Refactor common exception retry logic into RetryableExceptionHelper class.

---
+++ src/Common/FaultTolerance/RetryableExceptionHelper.cs
@@ -0,0 +1,125 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace KodeAid.FaultTolerance
+{
+    public static class RetryableExceptionHelper
+    {
+        public static bool CheckForRetryableHttpRequestException(Exception exception)
+        {
+            while (exception != null)
+            {
+                if (exception is HttpRequestException httpRequestException &&
+                    IsHttpRequestExceptionRetryable(httpRequestException))
+                {
+                    return true;
+                }
+                exception = exception.InnerException;
+            }
+
+            return false;
+        }
+
+        public static bool IsHttpRequestExceptionRetryable(HttpRequestException httpRequestException)
+        {
+            ArgCheck.NotNull(nameof(httpRequestException), httpRequestException);
+
+            // very ugly, but let's try to parse a 3-digit status code from the message.
+            var matches = Regex.Matches(httpRequestException.Message, @"(?:^|\D)(\d\d\d)(?:$|\D)", RegexOptions.Compiled);
+            if (matches.Count == 1)
+            {
+                var statusCodeAsString = matches[0].GetFirstInnerMostGroupCapture();
+                if (int.TryParse(statusCodeAsString, out var statusCode)
+                    && statusCode >= 500 && statusCode < 600)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CheckForRetryableSocketException(Exception exception)
+        {
+            while (exception != null)
+            {
+                if (exception is SocketException socketException &&
+                    IsSocketExceptionRetryable(socketException))
+                {
+                    return true;
+                }
+                exception = exception.InnerException;
+            }
+
+            return false;
+        }
+
+        public static bool IsSocketExceptionRetryable(SocketException socketException)
+        {
+            ArgCheck.NotNull(nameof(socketException), socketException);
+
+            switch (socketException.SocketErrorCode)
+            {
+                case SocketError.AccessDenied:
+                case SocketError.ConnectionAborted:
+                case SocketError.ConnectionRefused:
+                case SocketError.ConnectionReset:
+                case SocketError.Disconnecting:
+                case SocketError.Fault:
+                case SocketError.HostDown:
+                case SocketError.HostNotFound:
+                case SocketError.HostUnreachable:
+                case SocketError.InProgress:
+                case SocketError.Interrupted:
+                case SocketError.IOPending:
+                case SocketError.IsConnected:
+                case SocketError.MessageSize:
+                case SocketError.NetworkDown:
+                case SocketError.NetworkReset:
+                case SocketError.NetworkUnreachable:
+                case SocketError.NoBufferSpaceAvailable:
+                case SocketError.NoData:
+                case SocketError.NoRecovery:
+                case SocketError.NotConnected:
+                case SocketError.NotInitialized:
+                case SocketError.NotSocket:
+                case SocketError.OperationAborted:
+                case SocketError.ProcessLimit:
+                case SocketError.ProtocolOption:
+                case SocketError.Shutdown:
+                case SocketError.SocketError:
+                case SocketError.SystemNotReady:
+                case SocketError.TimedOut:
+                case SocketError.TooManyOpenSockets:
+                case SocketError.TryAgain:
+                case SocketError.TypeNotFound:
+                case SocketError.WouldBlock:
+                    return true;
+
+                case SocketError.AddressAlreadyInUse:
+                case SocketError.AddressFamilyNotSupported:
+                case SocketError.AddressNotAvailable:
+                case SocketError.AlreadyInProgress:
+                case SocketError.DestinationAddressRequired:
+                case SocketError.InvalidArgument:
+                case SocketError.OperationNotSupported:
+                case SocketError.ProtocolFamilyNotSupported:
+                case SocketError.ProtocolNotSupported:
+                case SocketError.ProtocolType:
+                case SocketError.SocketNotSupported:
+                case SocketError.Success:
+                case SocketError.VersionNotSupported:
+                default:
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
--- src/Common/Net/Http/HttpOperationManager.cs
+++ src/Common/Net/Http/HttpOperationManager.cs
@@ -34,108 +34,15 @@ namespace KodeAid.Net.Http
             }
 
             // look for a SocketException (root cause) and see if its retryable
-            var ex = exception;
-            while (ex != null)
+            if (RetryableExceptionHelper.CheckForRetryableSocketException(exception))
             {
-                if (ex is SocketException socketException &&
-                    CheckSocketExceptionIsRetryable(socketException))
-                {
-                    return true;
-                }
-                ex = ex.InnerException;
+                return true;
             }
 
             // look for an HttpRequestException (high-level) and see if its retryable
-            ex = exception;
-            while (ex != null)
+            if (RetryableExceptionHelper.CheckForRetryableHttpRequestException(exception))
             {
-                if (ex is HttpRequestException httpRequestException &&
-                    CheckHttpRequestExceptionIsRetryable(httpRequestException))
-                {
-                    return true;
-                }
-                ex = ex.InnerException;
-            }
-
-            return false;
-        }
-
-        public static bool CheckHttpRequestExceptionIsRetryable(HttpRequestException httpRequestException)
-        {
-            ArgCheck.NotNull(nameof(httpRequestException), httpRequestException);
-
-            // very ugly, but let's try to parse a 3-digit status code from the message.
-            var matches = Regex.Matches(httpRequestException.Message, @"(?:^|\D)(\d\d\d)(?:$|\D)", RegexOptions.Compiled);
-            if (matches.Count == 1)
-            {
-                var statusCodeAsString = matches[0].GetFirstInnerMostGroupCapture();
-                if (int.TryParse(statusCodeAsString, out var statusCode)
-                    && statusCode >= 500 && statusCode < 600)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static bool CheckSocketExceptionIsRetryable(SocketException socketException)
-        {
-            ArgCheck.NotNull(nameof(socketException), socketException);
-
-            switch (socketException.SocketErrorCode)
-            {
-                case SocketError.AccessDenied:
-                case SocketError.ConnectionAborted:
-                case SocketError.ConnectionRefused:
-                case SocketError.ConnectionReset:
-                case SocketError.Disconnecting:
-                case SocketError.Fault:
-                case SocketError.HostDown:
-                case SocketError.HostNotFound:
-                case SocketError.HostUnreachable:
-                case SocketError.InProgress:
-                case SocketError.Interrupted:
-                case SocketError.IOPending:
-                case SocketError.IsConnected:
-                case SocketError.MessageSize:
-                case SocketError.NetworkDown:
-                case SocketError.NetworkReset:
-                case SocketError.NetworkUnreachable:
-                case SocketError.NoBufferSpaceAvailable:
-                case SocketError.NoData:
-                case SocketError.NoRecovery:
-                case SocketError.NotConnected:
-                case SocketError.NotInitialized:
-                case SocketError.NotSocket:
-                case SocketError.OperationAborted:
-                case SocketError.ProcessLimit:
-                case SocketError.ProtocolOption:
-                case SocketError.Shutdown:
-                case SocketError.SocketError:
-                case SocketError.SystemNotReady:
-                case SocketError.TimedOut:
-                case SocketError.TooManyOpenSockets:
-                case SocketError.TryAgain:
-                case SocketError.TypeNotFound:
-                case SocketError.WouldBlock:
-                    return true;
-
-                case SocketError.AddressAlreadyInUse:
-                case SocketError.AddressFamilyNotSupported:
-                case SocketError.AddressNotAvailable:
-                case SocketError.AlreadyInProgress:
-                case SocketError.DestinationAddressRequired:
-                case SocketError.InvalidArgument:
-                case SocketError.OperationNotSupported:
-                case SocketError.ProtocolFamilyNotSupported:
-                case SocketError.ProtocolNotSupported:
-                case SocketError.ProtocolType:
-                case SocketError.SocketNotSupported:
-                case SocketError.Success:
-                case SocketError.VersionNotSupported:
-                default:
-                    break;
+                return true;
             }
 
             return false;
