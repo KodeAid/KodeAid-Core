26396f76160d1e4c0236f259032d585013822985
Kris Penner <krispenner@hotmail.com>
2019-04-22 21:53:40 -0700

    Add enum parser helpers that take into account EnumMemberAttribute.

--- src/Common/EnumHelper.cs
+++ src/Common/EnumHelper.cs
@@ -14,7 +14,7 @@ namespace KodeAid
     public static class EnumHelper
     {
         public static IEnumerable<TEnum> GetValues<TEnum>(bool includeZero = false, bool singleBitValuesOnly = false)
-            where TEnum : Enum, IConvertible
+            where TEnum : struct, Enum, IConvertible
         {
             foreach (var v in Enum.GetValues(typeof(TEnum))
                 .Cast<TEnum>()
@@ -29,25 +29,80 @@ namespace KodeAid
         /// <summary>
         /// Normalizes an string representation of an enumeration by replacing any occurences of <see cref="EnumMemberAttribute"/> with their corresponding constant name.
         /// </summary>
-        public static string NormalizeName<TEnum>(string str, bool ignoreCase = false, bool ignoreWhitespace = false, string flagsDelimiter = ", ")
-            where TEnum : Enum, IConvertible
+        public static string NormalizeName<TEnum>(string value, bool ignoreCase = false, bool ignoreWhitespace = false, string flagsDelimiter = ", ")
+            where TEnum : struct, Enum
         {
-            return NormalizeName(Enum<TEnum>.Info, str, ignoreCase, ignoreWhitespace, flagsDelimiter);
+            return NormalizeName(Enum<TEnum>.Info, value, ignoreCase, ignoreWhitespace, flagsDelimiter);
         }
 
         /// <summary>
         /// Normalizes an string representation of an enumeration by replacing any occurences of <see cref="EnumMemberAttribute"/> with their corresponding constant name.
         /// </summary>
-        public static string NormalizeName(Type enumType, string str, bool ignoreCase = false, bool ignoreWhitespace = false, string flagsDelimiter = ", ")
+        public static string NormalizeName(Type enumType, string value, bool ignoreCase = false, bool ignoreWhitespace = false, string flagsDelimiter = ", ")
         {
-            return NormalizeName(GetEnumInfo(enumType), str, ignoreCase, ignoreWhitespace, flagsDelimiter);
+            return NormalizeName(GetEnumInfo(enumType), value, ignoreCase, ignoreWhitespace, flagsDelimiter);
         }
 
+        /// <summary>
+        /// Get a string name of the enum, this will be the serialized name 
+        /// if one was explicitly set via an <see cref="EnumMemberAttribute"/> attribute,
+        /// otherwise it will be the normal enum field name.
+        /// </summary>
+        /// <typeparam name="TEnum">The enumeration type.</typeparam>
+        /// <param name="value">The enumeration constant value.</param>
+        /// <returns>The value's serialized name if it has one, otherwise the normal enum field name.</returns>
         public static string GetSerializedName<TEnum>(TEnum value)
-            where TEnum : Enum, IConvertible
+            where TEnum : struct, Enum
         {
-            var name = Enum<TEnum>.Info.Names.FirstOrDefault(n => value.CompareTo(n.Value) == 0);
-            return name.SerializedName ?? name.Name;
+            var (Value, Name, SerializedName) = Enum<TEnum>.Info.Names.FirstOrDefault(n => value.CompareTo(n.Value) == 0);
+            return SerializedName ?? Name;
+        }
+
+        /// <summary>
+        /// Converts the string representation of the name, serialized name, or numeric value 
+        /// of one or more enumerated constants to an equivalent enumerated object. 
+        /// A parameter specifies whether the operation is case-insensitive.
+        /// </summary>
+        /// <typeparam name="TEnum">The enumeration type to which to convert value.</typeparam>
+        /// <param name="value">The string representation of the enumeration name, serialized name, or underlying value to convert.</param>
+        /// <param name="ignoreCase">true to ignore case; false to consider case.</param>
+        /// <returns>An object of type enumType whose value is represented by value.</returns>
+        public static TEnum Parse<TEnum>(string value, bool ignoreCase = false)
+            where TEnum : struct, Enum
+        {
+            if (Enum.TryParse<TEnum>(value, ignoreCase, out var result))
+            {
+                return result;
+            }
+
+            value = NormalizeName<TEnum>(value, ignoreCase, true);
+            return (TEnum)Enum.Parse(typeof(TEnum), value, ignoreCase);
+        }
+
+        /// <summary>
+        /// Converts the string representation of the name, serialized name, or numeric value 
+        /// of one or more enumerated constants to an equivalent enumerated object. 
+        /// A parameter specifies whether the operation is case-insensitive.
+        /// </summary>
+        /// <typeparam name="TEnum">The enumeration type to which to convert value.</typeparam>
+        /// <param name="value">The string representation of the enumeration name, serialized name, or underlying value to convert.</param>
+        /// <param name="ignoreCase">true to ignore case; false to consider case.</param>
+        /// <param name="result">When this method returns, result contains an object of type TEnum whose value
+        /// is represented by value if the parse operation succeeds. If the parse operation
+        /// fails, result contains the default value of the underlying type of TEnum. Note
+        /// that this value need not be a member of the TEnum enumeration. This parameter
+        /// is passed uninitialized.</param>
+        /// <returns>true if the value parameter was converted successfully; otherwise, false.</returns>
+        public static bool TryParse<TEnum>(string value, out TEnum result, bool ignoreCase = false)
+            where TEnum : struct, Enum
+        {
+            if (Enum.TryParse(value, ignoreCase, out result))
+            {
+                return true;
+            }
+
+            value = NormalizeName<TEnum>(value, ignoreCase, true);
+            return Enum.TryParse(value, ignoreCase, out result);
         }
 
         private static string NormalizeName(EnumInfo enumInfo, string str, bool ignoreCase, bool ignoreWhitespace, string flagsDelimiter)
