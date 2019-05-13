fa12909bec92e4fa396aa4db41d6ba73f8825267
Kris Penner <krispenner@hotmail.com>
2019-05-13 15:17:08 -0700

    Expose throwOnError parameter when loading types for plug-ins.

--- src/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/DependencyInjection/ServiceCollectionExtensions.cs
@@ -18,9 +18,9 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="startingPoint">Assemblies to start the search from, if not provided the entry assembly is used: Assembly.GetEntryAssembly().</param>
         /// <param name="assemblySearchOptions">How to search for additional assemblies to include.</param>
         /// <param name="assemblyNamePrefixes">Case insensitive prefixes of assembly names and file names (*.dlls) to include in search, null/empty to include all.</param>
-        public static void AddRegisteredServices(this IServiceCollection services, Assembly startingPoint = null, AssemblySearchOptions assemblySearchOptions = AssemblySearchOptions.Default, params string[] assemblyNamePrefixes)
+        public static void AddRegisteredServices(this IServiceCollection services, Assembly startingPoint = null, AssemblySearchOptions assemblySearchOptions = AssemblySearchOptions.Default, bool throwOnError = false, params string[] assemblyNamePrefixes)
         {
-            var registrationTypes = ReflectionHelper.FindAllTypes<IServiceRegistration>(startingPoint: startingPoint, assemblySearchOptions: assemblySearchOptions, assemblyNamePrefixes: assemblyNamePrefixes);
+            var registrationTypes = ReflectionHelper.FindAllTypes<IServiceRegistration>(startingPoint: startingPoint, assemblySearchOptions: assemblySearchOptions, throwOnError: throwOnError, assemblyNamePrefixes: assemblyNamePrefixes);
             foreach (var registrationType in registrationTypes)
             {
                 var registration = (IServiceRegistration)Activator.CreateInstance(registrationType);
