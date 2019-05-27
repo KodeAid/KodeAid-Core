90b153d05c0e283e9c1448110567e641f3240302
Kris Penner <krispenner@hotmail.com>
2019-05-27 13:13:17 -0700

    Add InsertRange and Replace list extension methods.

--- src/Common/_Extensions/System/Collections/Generic/ListExtensions.cs
+++ src/Common/_Extensions/System/Collections/Generic/ListExtensions.cs
@@ -11,9 +11,42 @@ namespace System.Collections.Generic
         public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
         {
             ArgCheck.NotNull(nameof(list), list);
+
             if (items != null)
+            {
                 foreach (var item in items)
+                {
                     list.Add(item);
+                }
+            }
+        }
+
+        public static void InsertRange<T>(this IList<T> list, int index, IEnumerable<T> items)
+        {
+            ArgCheck.NotNull(nameof(list), list);
+
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    list.Insert(index++, item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the list and then adds the specificed items.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        /// <param name="allowNulls"></param>
+        public static void Replace<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            ArgCheck.NotNull(nameof(list), list);
+
+            list.Clear();
+            list.AddRange(items);
         }
     }
 }
