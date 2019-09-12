a002549b10643ec155ccadd26a05d26ff86e20aa
Kris Penner <krispenner@hotmail.com>
2019-09-11 21:03:53 -0700

    Add GetOrAdd() dictionary extension method.

--- src/Common/_Extensions/System/Collections/Generic/DictionaryExtensions.cs
+++ src/Common/_Extensions/System/Collections/Generic/DictionaryExtensions.cs
@@ -11,9 +11,40 @@ namespace System.Collections.Generic
         public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default)
         {
             ArgCheck.NotNull(nameof(dictionary), dictionary);
+
             if (dictionary.TryGetValue(key, out var value))
+            {
                 return value;
+            }
+
             return defaultValue;
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            ArgCheck.NotNull(nameof(dictionary), dictionary);
+
+            if (!dictionary.TryGetValue(key, out var v))
+            {
+                v = value;
+                dictionary.Add(key, v);
+            }
+            
+            return v;
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> valueFactory)
+        {
+            ArgCheck.NotNull(nameof(dictionary), dictionary);
+            ArgCheck.NotNull(nameof(valueFactory), valueFactory);
+
+            if (!dictionary.TryGetValue(key, out var value))
+            {
+                value = valueFactory(key);
+                dictionary.Add(key, value);
+            }
+
+            return value;
         }
     }
 }
