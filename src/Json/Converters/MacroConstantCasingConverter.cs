6fa8e3fc2aa5c21d0ea99316832c9e0dbc9d8ed2
Kris Penner <krispenner@hotmail.com>
2019-09-17 00:02:08 -0700

    Add PascalCasing and MacroConstantCasing JSON converters.

---
+++ src/Json/Converters/MacroConstantCasingConverter.cs
@@ -0,0 +1,89 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace KodeAid.Json.Converters
+{
+    public class MacroConstantCasingConverter : JsonConverter
+    {
+        public override bool CanConvert(Type type)
+        {
+            return type == typeof(string) || typeof(IEnumerable<string>).IsAssignableFrom(type) || (Nullable.GetUnderlyingType(type) ?? type).IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type type, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            type = Nullable.GetUnderlyingType(type) ?? type;
+            var flags = type.IsEnum && type.GetCustomAttributes(typeof(FlagsAttribute), false).Length == 1;
+            var array = flags || typeof(IEnumerable<string>).IsAssignableFrom(type);
+            var value = serializer.Deserialize(reader, array ? typeof(string[]) : typeof(string));
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (flags)
+            {
+                return Enum.Parse(type, string.Join(",", ((string[])value).Select(s => s.Replace("_", ""))), true);
+            }
+
+            if (type.IsEnum)
+            {
+                return Enum.Parse(type, ((string)value).Replace("_", ""), true);
+            }
+
+            if (array)
+            {
+                return ((string[])value).Select(s => s.ToMacroConstantCase()).ToList();
+            }
+
+            return ((string)value).ToMacroConstantCase();
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanWrite => true;
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            var type = value.GetType();
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            if (type.IsEnum)
+            {
+                if (type.GetCustomAttributes(typeof(FlagsAttribute), false).Length == 1)
+                {
+                    value = value.ToString().Split(',').Select(s => s.Trim()).ToArray();
+                }
+                else
+                {
+                    value = value.ToString();
+                }
+
+                type = value.GetType();
+            }
+
+            if (typeof(IEnumerable<string>).IsAssignableFrom(type))
+            {
+                value = ((IEnumerable<string>)value).Select(v => v?.ToString().ToMacroConstantCase()).ToArray();
+            }
+            else
+            {
+                value = value?.ToString().ToMacroConstantCase();
+            }
+
+            serializer.Serialize(writer, value);
+        }
+    }
+}
---
+++ src/Json/Converters/PascalCasingConverter.cs
@@ -0,0 +1,51 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace KodeAid.Json.Converters
+{
+    public class PascalCasingConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var value = reader.Value?.ToString();
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            // TODO: replace all the below with a regex
+
+            var hasInvalid = value.Any(c => !char.IsLetterOrDigit(c) && c != ' ' && c != '-' && c != '_' && c != '.');
+            if (hasInvalid)
+            {
+                return value;
+            }
+
+            var hasLetters = value.Any(c => char.IsLetter(c));
+            if (!hasLetters)
+            {
+                return value;
+            }
+
+            // if it has any lower case letters then assume it is already trying to do some form of camel/pascal casing and take this into account
+            var hasLowerCase = value.Any(c => char.IsLetter(c) && char.IsLower(c));
+
+            return string.Join("", value.Split(' ', '-', '_', '.').Where(s => s.Length > 0).Select(s => hasLowerCase && s.Length <= 2 ? s : (char.ToUpperInvariant(s[0]) + s.Substring(1).ToLowerInvariant())));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            writer.WriteValue(value);
+        }
+    }
+}
