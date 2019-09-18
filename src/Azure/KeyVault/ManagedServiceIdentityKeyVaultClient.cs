8340d0969053ae2ab41ce83ba5b36351f64ab59c
Kris Penner <krispenner@hotmail.com>
2019-09-17 23:36:22 -0700

    Use static HttpClient in KeyVault client to minimize sock exhaustion and sockets still open to the key vault store due to possible bug in Net Core.

--- src/Azure/KeyVault/ManagedServiceIdentityKeyVaultClient.cs
+++ src/Azure/KeyVault/ManagedServiceIdentityKeyVaultClient.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 
+using System.Net.Http;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 
@@ -11,10 +12,16 @@ namespace KodeAid.Azure.KeyVault
     {
         private static readonly AzureServiceTokenProvider _tokenProvider = new AzureServiceTokenProvider();
 
-        public ManagedServiceIdentityKeyVaultClient()
-            : base(new AuthenticationCallback(_tokenProvider.KeyVaultTokenCallback))
-        {
+        // Due to an issue with the internal HttpClient being left open or not disposed of properly,
+        // this shuold help.
+        // https://github.com/Azure/azure-sdk-for-net/issues/6003
+        // Although the only issue now is that DNS changes may not be noticed on the key vault hosts.
+        // https://softwareengineering.stackexchange.com/questions/330364/should-we-create-a-new-single-instance-of-httpclient-for-all-requests
+        private static readonly HttpClient _httpClient = new HttpClient();
 
+        public ManagedServiceIdentityKeyVaultClient()
+            : base(new AuthenticationCallback(_tokenProvider.KeyVaultTokenCallback), _httpClient)
+        {
         }
     }
 }
