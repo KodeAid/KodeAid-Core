328ec7366645ff30f9f29f9036da6ca41a846915
Kris Penner <krispenner@hotmail.com>
2019-11-05 13:31:02 -0800

    Add KodeAid.Http project.

---
+++ src/Http/HttpClientFactoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System.Net.Http;
+
+namespace Microsoft.Extensions.Http
+{
+    public static class HttpClientFactoryExtensions
+    {
+        public static HttpClient CreateClient<TClient>(this IHttpClientFactory httpClientFactory)
+        {
+            return httpClientFactory.CreateClient(typeof(TClient).FullName);
+        }
+    }
+}
