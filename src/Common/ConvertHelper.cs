4be6c9ab3ad9cd4f92a45e9192c0e2c6ce87c70b
Kris Penner <krispenner@hotmail.com>
2019-11-11 20:52:29 -0800

    Add ConvertHelper.TryConvert(ref obj, type)

--- src/Common/ConvertHelper.cs
+++ src/Common/ConvertHelper.cs
@@ -22,7 +22,10 @@ namespace KodeAid
         public static object Convert(object value, Type targetType)
         {
             if (TryConvert(value, targetType, out var result))
+            {
                 return result;
+            }
+
             // calling this again just to throw an error
             return System.Convert.ChangeType(value, targetType);
         }
@@ -35,10 +38,17 @@ namespace KodeAid
         public static object ConvertOrDefault(object value, Type targetType, object defaultValue = null)
         {
             ArgCheck.NotNull(nameof(targetType), targetType);
+
             if (defaultValue != null && !targetType.IsAssignableFrom(defaultValue.GetType()))
+            {
                 throw new ArgumentException($"Parameter '{nameof(defaultValue)}' must be assignable to '{nameof(targetType)}'.", nameof(defaultValue));
+            }
+
             if (TryConvert(value, targetType, out var result))
+            {
                 return result;
+            }
+
             return defaultValue;
         }
 
@@ -49,8 +59,14 @@ namespace KodeAid
                 result = (T)r;
                 return true;
             }
+
             result = default;
             return false;
+        }
+
+        public static bool TryConvert(ref object value, Type targetType)
+        {
+            return TryConvert(value, targetType, out value);
         }
 
         public static bool TryConvert(object value, Type targetType, out object result)
@@ -62,34 +78,45 @@ namespace KodeAid
                 if (value == null)
                 {
                     result = default;
+
                     if (targetType.GetTypeInfo().IsValueType)
+                    {
                         return false;
+                    }
+
                     return true;
                 }
+
                 var sourceType = value.GetType();
                 sourceType = Nullable.GetUnderlyingType(sourceType) ?? sourceType;
                 targetType = Nullable.GetUnderlyingType(targetType) ?? targetType;
+
                 if (sourceType == targetType || targetType.IsAssignableFrom(sourceType))
                 {
                     result = value;
                     return true;
                 }
+
                 if (value is string && string.IsNullOrEmpty((string)value))
                 {
                     result = default;
                     return false;
                 }
+
                 if (targetType == typeof(bool) && TryConvertBoolean(value, out var b))
                 {
                     result = b;
                     return true;
                 }
+
                 var typeConverter = TypeDescriptor.GetConverter(targetType);
+
                 if (typeConverter.CanConvertFrom(sourceType))
                 {
                     result = typeConverter.ConvertFrom(value);
                     return true;
                 }
+
                 result = System.Convert.ChangeType(value, targetType);
                 return true;
             }
@@ -97,6 +124,7 @@ namespace KodeAid
 
             // failed to convert
             result = default;
+
             return false;
         }
 
@@ -107,27 +135,33 @@ namespace KodeAid
                 result = false;
                 return true;
             }
+
             if (value is bool)
             {
                 result = (bool)value;
                 return true;
             }
+
             if (value is bool?)
             {
                 result = ((bool?)value).GetValueOrDefault();
                 return true;
             }
+
             if (_trueStringsForBooleanConversion.Contains(value.ToString().ToLowerInvariant()))
             {
                 result = true;
                 return true;
             }
+
             if (_falseStringsForBooleanConversion.Contains(value.ToString().ToLowerInvariant()))
             {
                 result = false;
                 return true;
             }
+
             result = default;
+
             return false;
         }
     }
