219d4be86157118e901c08cc01a1ca72f32d42a5
Kris Penner <krispenner@hotmail.com>
2019-12-20 13:21:43 -0800

    Add DateTimeAsDate and DateTimeOffsetType JSON converters.

---
+++ src/Json/Converters/DateTimeTypeAsDateConverter.cs
@@ -0,0 +1,27 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace KodeAid.Json.Converters
+{
+    /// <summary>
+    /// Converts any DateTime to/from a date only JSON string formatted value.
+    /// Default format of "yyyy'-'MM'-'dd'".
+    /// </summary>
+    public class DateTimeTypeAsDateConverter : DateTimeConverter
+    {
+        public DateTimeTypeAsDateConverter()
+            : this("yyyy'-'MM'-'dd'")
+        {
+        }
+
+        public DateTimeTypeAsDateConverter(string format)
+        {
+            ArgCheck.NotNull(nameof(format), format);
+
+            AllowDateTime = true;
+            AllowDateTimeOffset = false;
+            WriteDateTimeFormat = format;
+            ReadDateTimeFormats = new[] { format };
+        }
+    }
+}
