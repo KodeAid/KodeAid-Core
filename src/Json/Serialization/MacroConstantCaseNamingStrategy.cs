39070ef82b7b2c104432090afbb81468bc77a75d
Kris Penner <krispenner@hotmail.com>
2019-12-20 12:44:26 -0800

    Add MacroConstantCaseNamingStrategy.

---
+++ src/Json/Serialization/MacroConstantCaseNamingStrategy.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © Kris Penner. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using System;
+using Newtonsoft.Json.Serialization;
+
+namespace KodeAid.Json.Serialization
+{
+    public class MacroConstantCaseNamingStrategy : CamelCaseNamingStrategy
+    {
+        public MacroConstantCaseNamingStrategy()
+            : base()
+        {
+
+        }
+
+        public MacroConstantCaseNamingStrategy(bool processDictionaryKeys, bool overrideSpecifiedNames)
+            : base(processDictionaryKeys, overrideSpecifiedNames)
+        {
+
+        }
+
+        public MacroConstantCaseNamingStrategy(bool processDictionaryKeys, bool overrideSpecifiedNames, bool processExtensionDataNames)
+            : base(processDictionaryKeys, overrideSpecifiedNames, processExtensionDataNames)
+        {
+
+        }
+
+        protected override string ResolvePropertyName(string name) => base.ResolvePropertyName(name)?.ToMacroConstantCase();
+    }
+}
