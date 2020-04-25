1692fb888fe1600459e792992a29262fde7b450c
Kris Penner <krispenner@hotmail.com>
2020-04-24 20:03:03 -0700

    Optionally include non-public types in IServiceRegistration searches across assemblies.

--- src/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/DependencyInjection/ServiceCollectionExtensions.cs
@@ -16,11 +16,20 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="services">The services collection.</param>
         /// <param name="startingPoint">Assemblies to start the search from, if not provided the entry assembly is used: Assembly.GetEntryAssembly().</param>
+        /// <param name="includeNonPublic">True to include non-public types in the search, otherwise false to search only public types.</param>
         /// <param name="assemblySearchOptions">How to search for additional assemblies to include.</param>
         /// <param name="assemblyNamePrefixes">Case insensitive prefixes of assembly names and file names (*.dlls) to include in search, null/empty to include all.</param>
-        public static IServiceCollection AddRegisteredServices(this IServiceCollection services, Assembly startingPoint = null, AssemblySearchOptions assemblySearchOptions = AssemblySearchOptions.Default, bool throwOnError = false, params string[] assemblyNamePrefixes)
+        public static IServiceCollection AddRegisteredServices(this IServiceCollection services, Assembly startingPoint = null, bool includeNonPublic = false, AssemblySearchOptions assemblySearchOptions = AssemblySearchOptions.Default, bool throwOnError = false, params string[] assemblyNamePrefixes)
         {
-            var registrationTypes = ReflectionHelper.FindAllTypes<IServiceRegistration>(startingPoint: startingPoint, assemblySearchOptions: assemblySearchOptions, throwOnError: throwOnError, assemblyNamePrefixes: assemblyNamePrefixes);
+            static bool nonPublicTypeFilter(Type t) => (t.IsClass || t.IsValueType) && !t.IsAbstract && !t.IsGenericType;
+            static bool publicTypeFilter(Type t) => (t.IsClass || t.IsValueType) && t.IsPublic && !t.IsAbstract && !t.IsGenericType;
+
+            var registrationTypes = ReflectionHelper.FindAllTypes<IServiceRegistration>(
+                startingPoint: startingPoint,
+                typeFilter: includeNonPublic ? nonPublicTypeFilter : (Predicate<Type>)publicTypeFilter,
+                assemblySearchOptions: assemblySearchOptions,
+                throwOnError: throwOnError,
+                assemblyNamePrefixes: assemblyNamePrefixes);
 
             foreach (var registrationType in registrationTypes)
             {
