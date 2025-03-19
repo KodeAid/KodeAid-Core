724cca83b20d442bf50de021e8edf77a979455a0
Kris <kris@wealthly.ca>
2025-03-18 21:06:39 -0700

    More collection extension methods.

--- src/Common/_Extensions/System/Collections/Generic/CollectionExtensions.cs
+++ src/Common/_Extensions/System/Collections/Generic/CollectionExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 
+using System.Linq;
 using KodeAid;
 
 namespace System.Collections.Generic
@@ -11,17 +12,46 @@ namespace System.Collections.Generic
         public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
             ArgCheck.NotNull(nameof(collection), collection);
+
             if (items != null)
+            {
                 foreach (var item in items)
+                {
                     collection.Add(item);
+                }
+            }
         }
 
         public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
             ArgCheck.NotNull(nameof(collection), collection);
+
             if (items != null)
+            {
                 foreach (var item in items)
+                {
                     collection.Remove(item);
+                }
+            }
+        }
+
+        public static void ForEach<T>(this ICollection<T> collection, Action<T> action)
+        {
+            ArgCheck.NotNull(nameof(collection), collection);
+            ArgCheck.NotNull(nameof(action), action);
+
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+
+        public static void RemoveAll<T>(this ICollection<T> collection, Predicate<T> match)
+        {
+            ArgCheck.NotNull(nameof(collection), collection);
+            ArgCheck.NotNull(nameof(match), match);
+
+            collection.RemoveRange(collection.Where(item => match(item)).ToList());
         }
     }
 }
